26542ddffa389ea932ab3a4e0a7a6ecf21bd653f
Harshal Jain <harshaljn04@gmail.com>
2023-06-22 23:32:37 +0530

    Add files via upload

---
+++ buttons_checks.js
@@ -0,0 +1,25 @@
+/*let count=0;
+
+document.getElementById("decbtn").onclick = function(){
+    count-=1;
+    document.getElementById("countlabel").innerHTML=count;
+}
+
+document.getElementById("resbtn").onclick = function(){
+    count=0;
+    document.getElementById("countlabel").innerHTML=count;
+}
+
+document.getElementById("incbtn").onclick = function(){
+    count+=1;
+    document.getElementById("countlabel").innerHTML=count;
+}*/
+
+/*document.getElementById("mybutton").onclick = function(){
+    if(document.getElementById("mcb").checked==true){
+        console.log("You are subscribed");
+    }
+    else{
+        console.log("You are not subscribed");
+    }
+}*/
